3a926287d1436dd5490b403df23cdc592fec5ecf
eanjc <31427165+eanjc@users.noreply.github.com>
2018-05-09 02:28:24 +0800

    一部分基准线字符分割算法，待添加实验效果

---
+++ matlab/char_cut.m
@@ -0,0 +1,45 @@
+%-----------------------------------------
+no=261;
+uint8si=aftrnimglist{no};
+dbsi=im2double(uint8si);
+P=[];
+for x=1:200
+    c=0;
+    for y=1:50
+        if dbsi(y,x)==0
+            c=c+1;
+        end
+    end
+    P(x)=c;
+end
+
+figure;
+%subplot(1,2,1);
+imshow(dbsi);
+axis on;
+figure;
+%subplot(1,2,2);
+plot(P);
+%-----------------------------------------
+tb=zeros(1,200);
+for i=1:500
+    ig=aftrnimglist{i};
+    %pp=[];
+    for x=1:200
+        c=0;
+        for y=1:50
+            if ig(y,x)==0;
+                c=c+1;
+            end
+        end
+        %pp(x)=c;
+        if c==0;
+            tb(x)=tb(x)+1;
+        end
+    end
+end
+figure;
+plot(tb);
+
+[yp,idxp]=findpeaks(tb);
+    
--- matlab/loadImgs.m
+++ matlab/loadImgs.m
@@ -1,7 +1,12 @@
 grayimglist={};
+aftrnimglist={};
 for i =1 : 500
     str=sprintf('C:/文件/毕业设计/code/imgVerify/img/source/sourceimg%d.jpg',i);
+    str2=sprintf('C:/文件/毕业设计/test/18MF-9/MF%d.jpg',i);
     img=imread(str);
+    img2=imread(str2);
     grayimg=rgb2gray(img);
+    %bwimg=rgb2gray(img2);
     grayimglist{i}=grayimg;
+    aftrnimglist{i}=img2;
 end;
